0d5254752d8e4cc139c1ef0f2a5977e29d38d801
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-07-22 20:52:02 +0530

    Increased latency (avoids neomutt flicker)

--- st-0.8.4/config.def.h
+++ st-0.8.4/config.def.h
@@ -53,8 +53,8 @@ int allowwindowops = 0;
  * near minlatency, but it waits longer for slow updates to avoid partial draw.
  * low minlatency will tear/flicker more, as it can "detect" idle too early.
  */
-static double minlatency = 8;
-static double maxlatency = 33;
+static double minlatency = 15;
+static double maxlatency = 40;
 
 /*
  * blinking timeout (set to 0 to disable blinking) for the terminal blinking
