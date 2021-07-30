bd8912b638c8140910c18480054ab7e71217b94c
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-07-30 12:36:04 +0530

    Fixed selection clearing on resize
    Even if the horizontal size is increasing, selection should be cleared
    if it spans across lines, otherwise it would get corrupted.

--- st/st.c
+++ st/st.c
@@ -2656,10 +2656,8 @@ tresize(int col, int row)
 
 	if (sel.mode != SEL_EMPTY && sel.ob.x != -1 &&
 	    ((sel.type == SEL_RECTANGULAR && sel.ne.x >= col) ||
-	     ((col < term.col && sel.ne.y != sel.nb.y) ||
-	      (sel.nb.x >= col || sel.ne.x >= col)))) {
+	     (sel.ne.y != sel.nb.y || sel.nb.x >= col || sel.ne.x >= col)))
 		selclear();
-	}
 
 	/* switch to non-alt screen if on alt screen */
 	if (alt) {
