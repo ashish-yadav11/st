8da8f3841cae80e6b16330f6cb5818e188bcffc8
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-08-01 03:06:34 +0530

    Swapped selmasks

--- st/config.def.h
+++ st/config.def.h
@@ -170,7 +170,7 @@ static unsigned int defaultattr = 11;
  * Note that if you want to use ShiftMask with selmasks, set this to an other
  * modifier, set to 0 to not use it.
  */
-static uint forcemousemod = ControlMask;
+static uint forcemousemod = ShiftMask;
 
 /*
  * Internal mouse shortcuts.
@@ -473,7 +473,7 @@ static Key key[] = {
  * If no match is found, regular selection is used.
  */
 static uint selmasks[] = {
-	[SEL_RECTANGULAR] = ShiftMask,
+	[SEL_RECTANGULAR] = ControlMask,
 };
 
 /*
