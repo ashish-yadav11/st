a6ed88b509e64d0d8be28a0e0344d779d4034a4c
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2021-12-27 23:51:22 +0530

    Reorderd glyph_attributes

--- st/st.h
+++ st/st.h
@@ -22,20 +22,20 @@
 
 enum glyph_attribute {
 	ATTR_NULL       = 0,
-	ATTR_SET        = 1 << 0,
-	ATTR_BOLD       = 1 << 1,
-	ATTR_FAINT      = 1 << 2,
-	ATTR_ITALIC     = 1 << 3,
-	ATTR_UNDERLINE  = 1 << 4,
-	ATTR_BLINK      = 1 << 5,
-	ATTR_REVERSE    = 1 << 6,
-	ATTR_INVISIBLE  = 1 << 7,
-	ATTR_STRUCK     = 1 << 8,
-	ATTR_WRAP       = 1 << 9,
-	ATTR_WIDE       = 1 << 10,
-	ATTR_WDUMMY     = 1 << 11,
-	ATTR_SELECTED   = 1 << 12,
+	ATTR_BOLD       = 1 << 0,
+	ATTR_FAINT      = 1 << 1,
+	ATTR_ITALIC     = 1 << 2,
+	ATTR_UNDERLINE  = 1 << 3,
+	ATTR_BLINK      = 1 << 4,
+	ATTR_REVERSE    = 1 << 5,
+	ATTR_INVISIBLE  = 1 << 6,
+	ATTR_STRUCK     = 1 << 7,
+	ATTR_SELECTED   = 1 << 8,
 	ATTR_BOLD_FAINT = ATTR_BOLD | ATTR_FAINT,
+	ATTR_SET        = 1 << 9,
+	ATTR_WRAP       = 1 << 10,
+	ATTR_WIDE       = 1 << 11,
+	ATTR_WDUMMY     = 1 << 12,
 };
 
 enum selection_mode {
