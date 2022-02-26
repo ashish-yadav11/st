3df2c4893eeb0ec4342e6416e50bae8100337193
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2022-02-27 02:14:48 +0530

    Upstream update e823e2308f2a99023032a3966ebb7036a31d305f
    Delay redrawals on palette changes
    
    Build on auto-sync and only mark window dirty on palette changes and let
    the event handler do the actual draw.

--- st/st.c
+++ st/st.c
@@ -2291,7 +2291,7 @@ strhandle(void)
 			} else if (xsetcolorname(defaultfg, p)) {
 				fprintf(stderr, "erresc: invalid foreground color : %s\n", p);
 			} else {
-				redraw();
+				tfulldirt();
 			}
 			return;
 		case 11:
@@ -2303,7 +2303,7 @@ strhandle(void)
 			} else if (xsetcolorname(defaultbg, p)) {
 				fprintf(stderr, "erresc: invalid background color: %s\n", p);
 			} else {
-				redraw();
+				tfulldirt();
 			}
 			return;
 		case 12:
@@ -2315,7 +2315,7 @@ strhandle(void)
 			} else if (xsetcolorname(defaultcs, p)) {
 				fprintf(stderr, "erresc: invalid cursor color: %s\n", p);
 			} else {
-				redraw();
+				tfulldirt();
 			}
 			return;
 		case 4: /* color set */
@@ -2337,7 +2337,7 @@ strhandle(void)
 				 * TODO: if defaultbg color is changed, borders
 				 * are dirty
 				 */
-				redraw();
+				tfulldirt();
 			}
 			return;
 		}
