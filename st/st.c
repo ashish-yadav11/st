0106a14a5f332dd41b14c7fd0bb48e2358de8642
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2022-09-22 09:38:30 +0530

    Increased HISTSIZE to 10000

--- st/st.c
+++ st/st.c
@@ -39,8 +39,8 @@
 #define ESC_ARG_SIZ   16
 #define STR_BUF_SIZ   ESC_BUF_SIZ
 #define STR_ARG_SIZ   ESC_ARG_SIZ
-#define HISTSIZE      2000
-#define RESIZEBUFFER  1000
+#define HISTSIZE      10000
+#define RESIZEBUFFER  (HISTSIZE / 2)
 
 /* macros */
 #define IS_SET(flag)		((term.mode & (flag)) != 0)
