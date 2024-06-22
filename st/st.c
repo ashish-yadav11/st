34954a4d8357ce1891428ee602f4fcd919ab4725
Ashish Kumar Yadav <ashishkumar.yadav@students.iiserpune.ac.in>
2024-06-23 05:14:34 +0530

    Upstream update 5dbcca49263be094fc38159c297458ae323ef647
    Author: Hiltjo Posthuma <hiltjo@codemadness.org>
    Date:   Wed May 1 20:45:39 2024 +0200
    
        support colons in SGR character attributes
    
        Patch by Mikhail Kot <to@myrrc.dev>
        With some modifications to behave more like xterm (see note below).
    
        Example:
    
                printf '\033[48;2;255:0:0mtest\n'
    
        https://invisible-island.net/xterm/ctlseqs/ctlseqs.html
    
        Some notes:
    
        "CSI Pm m  Character Attributes (SGR).
        [...]
        o   xterm allows either colons (standard) or semicolons
        (legacy) to separate the subparameters (but after the
        first colon, colons must be used).

--- st/st.c
+++ st/st.c
@@ -1449,6 +1449,7 @@ csiparse(void)
 {
 	char *p = csiescseq.buf, *np;
 	long int v;
+	int sep = ';'; /* colon or semi-colon, but not both */
 
 	csiescseq.narg = 0;
 	if (*p == '?') {
@@ -1466,7 +1467,9 @@ csiparse(void)
 			v = -1;
 		csiescseq.arg[csiescseq.narg++] = v;
 		p = np;
-		if (*p != ';' || csiescseq.narg == ESC_ARG_SIZ)
+		if (sep == ';' && *p == ':')
+			sep = ':'; /* allow override to colon once */
+		if (*p != sep || csiescseq.narg == ESC_ARG_SIZ)
 			break;
 		p++;
 	}
